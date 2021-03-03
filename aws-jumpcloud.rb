d609e25ebe39a38e6dcc4870348f2c161bc34789
action-homebrew-release <foo@bar.com>
2021-03-03 13:48:52 -0700

    bump

---
+++ aws-jumpcloud.rb
@@ -0,0 +1,32 @@
+class AwsJumpcloud < Formula
+  include Language::Python::Virtualenv
+
+  desc "aws-vault like tool for JumpCloud authentication"
+  homepage "https://github.com/CirrusMD/aws-jumpcloud"
+  url "https://github.com/CirrusMD/aws-jumpcloud/releases/download/v2.1.8/aws_jumpcloud-2.1.8.tar.gz"
+  version "v2.1.8"
+  sha256 "8ecca58e90d1e3da9e8bdd1182c8e3930fd6417f7cf3b72b925b95567432bebd"
+  head "https://github.com/CirrusMD/aws-jumpcloud.git", :branch => 'main'
+  depends_on "python"
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    system libexec/"bin/pip", "install", "-v", "--ignore-installed", buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "aws-jumpcloud"
+    venv.pip_install_and_link buildpath
+  end
+
+  def caveats; <<~EOS
+    Use aws-jumpcloud --help to see available commands
+
+    Check out the README to look into migrating existing ~/.aws credentials:
+
+      https://github.com/CirrusMD/aws-jumpcloud/blob/main/README.md#migrating-from-aws-credentials
+
+  EOS
+  end
+
+  test do
+    assert_match "rotate", shell_output("#{bin}/aws-jumpcloud help")
+  end
+end
