a881a35f8836299304dc2d18d43c5396bcf5231e
action-homebrew-release <action-homebrew-release@nowhere.local>
2021-09-15 17:07:58 +0000

    action-homebrew-release, version: v2.1.12

--- aws-jumpcloud.rb
+++ aws-jumpcloud.rb
@@ -3,9 +3,9 @@ class AwsJumpcloud < Formula
 
   desc "aws-vault like tool for JumpCloud authentication"
   homepage "https://github.com/CirrusMD/aws-jumpcloud"
-  url "https://github.com/CirrusMD/aws-jumpcloud/releases/download/v2.1.11/aws_jumpcloud-2.1.11.tar.gz"
-  version "v2.1.11"
-  sha256 "b3d50fe3928963ccbe7e9058e0becd809b532c958890e6660fe64bbe5c412cc0"
+  url "https://github.com/CirrusMD/aws-jumpcloud/releases/download/v2.1.12/aws_jumpcloud-2.1.11.tar.gz"
+  version "v2.1.12"
+  sha256 "26947ab680dcdea00db96dc7cca7844de911ea06f3e569ca4e4b7b9573475549"
   head "https://github.com/CirrusMD/aws-jumpcloud.git", :branch => 'main'
   depends_on "python@3.8"
 
